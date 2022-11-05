de00b004d50b12516ebf35edcbf39f1d0837bbc9
Woozy <106052832+anfrosus@users.noreply.github.com>
2022-11-05 20:13:58 +0900

    Feat: 게시글 crud 1차

--- src/main/java/com/sdy/bbbb/dto/request/PostRequestDto.java
+++ src/main/java/com/sdy/bbbb/dto/request/PostRequestDto.java
@@ -1,4 +1,18 @@
 package com.sdy.bbbb.dto.request;
 
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
 public class PostRequestDto {
+    @NotBlank
+    private String content;
+    @NotBlank
+    private String category;
+    private List<String> imageUrl;
+    private List<String> deleteUrl;
+    @NotBlank
+    private String gu;
 }
--- src/main/java/com/sdy/bbbb/util/Chrono.java
+++ src/main/java/com/sdy/bbbb/util/Chrono.java
@@ -1,4 +1,39 @@
 package com.sdy.bbbb.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Chrono {
+
+    public static long dPlus(LocalDateTime dayBefore) {
+        return ChronoUnit.DAYS.between(dayBefore, LocalDateTime.now());
+    }
+
+    public static long dMinus(LocalDateTime dayAfter) {
+        return ChronoUnit.DAYS.between(dayAfter, LocalDateTime.now());
+    }
+
+    public static String timesAgo(LocalDateTime dayBefore) {
+        long gap = ChronoUnit.MINUTES.between(dayBefore, LocalDateTime.now());
+        String word = "";
+        if (gap == 0){
+            word = "방금 전";
+        }else if (gap < 60) {
+            word = gap + "분 전";
+        }else if (gap < 60 * 24){
+            word = (gap / 60) + "시간 전";
+        }else if (gap < 60 * 24 * 10) {
+            word = (gap / 60 / 24) + "일 전";
+        }else if (gap < 60 * 24 * 7 * 5) {
+            word = (gap / 60 / 24 / 7) + "주 전";
+        }else {
+            word = dayBefore.format(DateTimeFormatter.ofPattern("MM월 dd일"));
+        }
+        return word;
+    }
+
+    public static String customForm(LocalDateTime date) {
+        return date.format(DateTimeFormatter.ofPattern("MM월 dd일"));
+    }
 }
