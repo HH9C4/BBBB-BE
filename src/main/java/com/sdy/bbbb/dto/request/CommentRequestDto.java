117b8d15b074ea2c9e34e08b94e4a22261b17bdf
Woozy <106052832+anfrosus@users.noreply.github.com>
2022-11-09 00:15:59 +0900

    Fix : 1차 테스트

--- src/main/java/com/sdy/bbbb/dto/request/CommentRequestDto.java
+++ src/main/java/com/sdy/bbbb/dto/request/CommentRequestDto.java
@@ -2,10 +2,12 @@ package com.sdy.bbbb.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class CommentRequestDto {
     @NotBlank
