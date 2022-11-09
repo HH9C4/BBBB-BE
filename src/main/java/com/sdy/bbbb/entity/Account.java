949d0993d6bfd909ceac5ce175d69930e7666714
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-11-09 21:45:35 +0900

    Update Account.java

--- src/main/java/com/sdy/bbbb/entity/Account.java
+++ src/main/java/com/sdy/bbbb/entity/Account.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
+@ApiIgnore // swagger 문서화 
 public class Account extends TimeStamped {
 
 
