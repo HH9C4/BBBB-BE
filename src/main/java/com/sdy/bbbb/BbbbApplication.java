edd136bd1a21d58df562c575ebc3a97d46d3100c
Woozy <106052832+anfrosus@users.noreply.github.com>
2022-11-10 19:31:20 +0900

    Fix: s3 dependency

--- src/main/java/com/sdy/bbbb/BbbbApplication.java
+++ src/main/java/com/sdy/bbbb/BbbbApplication.java
@@ -1,5 +1,6 @@
 package com.sdy.bbbb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BbbbApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(BbbbApplication.class, args);
     }
 
