bcce693f34a558322963ba7ec170f49692d44eb5
Younseo Jang <younseojj@gmail.com>
2022-11-10 17:10:53 +0900

    Fix : s3 업로더 경로 설정

--- src/main/java/com/sdy/bbbb/s3/S3Uploader.java
+++ src/main/java/com/sdy/bbbb/s3/S3Uploader.java
@@ -61,8 +61,7 @@ public class S3Uploader {
     }
 
     private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
-//        System.getProperty("user.dir") + "/" +
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
                 fos.write(file.getBytes());
