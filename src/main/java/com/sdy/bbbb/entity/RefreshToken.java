ef5c33d0313de8bd3571651314c168f26d743d15
Younseo Jang <younseojj@gmail.com>
2022-11-11 22:43:10 +0900

    Fix : erase cascade

--- src/main/java/com/sdy/bbbb/entity/RefreshToken.java
+++ src/main/java/com/sdy/bbbb/entity/RefreshToken.java
@@ -12,7 +12,7 @@ public class RefreshToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int refreshId;
+    private Long refreshId;
 
     @Column(nullable = false)
     private String refreshToken;
