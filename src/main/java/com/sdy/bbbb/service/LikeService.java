a440f0ee14b0ff47dc79758b026ba3e1a8b550df
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-11-14 22:11:22 +0900

    Feat : deleteLike Response 변경

--- src/main/java/com/sdy/bbbb/service/LikeService.java
+++ src/main/java/com/sdy/bbbb/service/LikeService.java
@@ -40,8 +40,6 @@ public class LikeService {
                 // 좋아요 저장
                 post.setLikeCount(post.getLikeCount() + 1);
                 // 게시글 좋아요 수 변경
-                postRepository.save(post);
-                // 게시글 저장
             }
             return GlobalResponseDto.created("success Likes!", new LikeResponseDto(amILiked(post, account), post.getLikeCount()));
         } // level이 1이 아니면 댓글 좋아요 생성
@@ -58,14 +56,12 @@ public class LikeService {
             // 좋아요 저장
             comment.setLikeCount(comment.getLikeCount() + 1);
             // 댓글 좋아요 수 변경
-            commentRepository.save(comment);
-            // 댓글 저장
         }
         return GlobalResponseDto.created("success Likes!", new LikeResponseDto(myLikedComment(comment, account), comment.getLikeCount()));
     }
 
     @Transactional
-    public GlobalResponseDto deletePostLike(Long id, Integer level, Account account){
+    public GlobalResponseDto<LikeResponseDto> deletePostLike(Long id, Integer level, Account account){
         // level이 1이면 게시글 좋아요 삭제
         if (level == 1) {
             Post post = postRepository.findById(id).orElseThrow(
@@ -78,9 +74,7 @@ public class LikeService {
                 // 좋아요 삭제
                 post.setLikeCount(post.getLikeCount() - 1);
                 // 게시글 좋아요 수 변경
-                postRepository.save(post);
-                // 게시글 저장
-            return GlobalResponseDto.ok("delete Likes!", null);
+            return GlobalResponseDto.ok("delete Likes!",  new LikeResponseDto(amILiked(post, account), post.getLikeCount()));
         } // level이 1이 아니면 댓글 좋아요 삭제
         Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.NotFoundPost));
@@ -92,10 +86,8 @@ public class LikeService {
             // 좋아요 삭제
             comment.setLikeCount(comment.getLikeCount() - 1);
             // 댓글 좋아요 수 변경
-            commentRepository.save(comment);
-            // 댓글 저장
 
-        return GlobalResponseDto.ok("delete Likes!", null);
+        return GlobalResponseDto.ok("delete Likes!", new LikeResponseDto(myLikedComment(comment, account), comment.getLikeCount()));
     }
 
     //좋아요 여부
