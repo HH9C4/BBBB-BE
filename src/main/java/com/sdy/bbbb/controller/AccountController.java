355f31cc30b29b6cb119afbfccc18cd4cac3f75e
Younseo Jang <younseojj@gmail.com>
2022-11-14 15:25:58 +0900

    Feat : logout 기능 추가

--- src/main/java/com/sdy/bbbb/controller/AccountController.java
+++ src/main/java/com/sdy/bbbb/controller/AccountController.java
@@ -1,12 +1,15 @@
 package com.sdy.bbbb.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sdy.bbbb.config.UserDetailsImpl;
 import com.sdy.bbbb.dto.response.GlobalResponseDto;
 import com.sdy.bbbb.dto.response.LoginResponseDto;
 import com.sdy.bbbb.service.AccountService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,11 @@ public class AccountController {
         return accountService.kakaoLogin(code, response);
     }
 
+    //로그아웃
+    @ApiOperation(value = "kakao logout api info", notes = "logout")
+    @DeleteMapping("api/logout")
+    public GlobalResponseDto<String> logout(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return accountService.logout(userDetails.getAccount());
+    }
 
 }
