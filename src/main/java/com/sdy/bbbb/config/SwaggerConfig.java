abc1df33bba2ff0f210e417ef0cce6994455b1bc
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-11-15 19:29:19 +0900

    Feat : swagger Docket 수정

--- src/main/java/com/sdy/bbbb/config/SwaggerConfig.java
+++ src/main/java/com/sdy/bbbb/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .ignoredParameterTypes(java.sql.Date.class)
                 .forCodeGeneration(true)
                 .select()
