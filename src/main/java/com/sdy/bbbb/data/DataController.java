66ac0947792026e2963961de66b6f4b85840c229
Woozy <106052832+anfrosus@users.noreply.github.com>
2022-11-18 00:37:55 +0900

    Feat: data statistics2
    Co-authored-by: Younseo Jang <younseojj@gmail.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/data/DataController.java
+++ src/main/java/com/sdy/bbbb/data/DataController.java
@@ -1,8 +1,12 @@
 package com.sdy.bbbb.data;
 
+import com.sdy.bbbb.dto.response.GlobalResponseDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,4 +18,11 @@ public class DataController {
     public void callApi() throws Exception {
         dataService.call();
     }
+
+    @GetMapping("/api/population")
+    @ApiOperation(value = "정보2번 실시간 혼잡도", notes = "내용입니다")
+    public GlobalResponseDto<List<PopulationChangesDto>> getPopulation() {
+        return dataService.getPopulationChanges();
+    }
+
 }
