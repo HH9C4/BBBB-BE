63af40f5cca41e2e6bf004162640a44c9017f137
Woozy <106052832+anfrosus@users.noreply.github.com>
2022-11-18 02:09:23 +0900

    Feat: data statistics2
    Co-authored-by: Younseo Jang <younseojj@gmail.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/data/PopulationDto.java
+++ src/main/java/com/sdy/bbbb/data/PopulationDto.java
@@ -4,14 +4,14 @@ package com.sdy.bbbb.data;
 
 public interface PopulationDto {
 
+    Integer getRow_Num();
+
+    String getArea_Congest_Lvl();
+
     String getArea_Nm();
 
     Long getPlus_Minus();
 
     Double getIncrease_Rate();
-
-    String getArea_Congest_Lvl();
-
-    Integer getRow_Num();
 
 }
