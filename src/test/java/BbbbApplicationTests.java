78fd48119d887e9cc4336de93c15df06ce4b4a4a
Younseo Jang <younseojj@gmail.com>
2022-11-18 22:06:36 +0900

    Feat : Sentry 적용 실험
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

---
+++ src/test/java/BbbbApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sdy.bbbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("안녕!");
+    }
+
+}
+
