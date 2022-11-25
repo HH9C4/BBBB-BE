c2ae98527ac6ff5b665e0a54002b0b3a14ce2491
Younseo Jang <younseojj@gmail.com>
2022-11-25 17:21:06 +0900

    Fix : 데이터 형식 변환
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/data/DataService.java
+++ src/main/java/com/sdy/bbbb/data/DataService.java
@@ -133,7 +133,7 @@ public class DataService {
             for(SpotCalculated spot1 : spotCalculateds) {
                 if (guBaseInfo.getArea_nm().equals(spot1.getArea_Nm())) {
 
-                    lastPopByHour.put("L" + spot1.getThat_Hour(), Long.parseLong(spot1.getPopulation_By_Hour()));
+                    lastPopByHour.put("L" + spot1.getThat_Hour(), (long)Double.parseDouble(spot1.getPopulation_By_Hour()));
 //                    mapList.add(popByHour);
                 }
             }
