2e5a4cd4fa5ba7897d027f93cad4530d25e956b4
Younseo Jang <younseojj@gmail.com>
2022-12-01 02:32:14 +0900

    Fix : close redis
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/redis/RedisConfig2.java
+++ src/main/java/com/sdy/bbbb/redis/RedisConfig2.java
@@ -1,38 +1,38 @@
-package com.sdy.bbbb.redis;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class RedisConfig2 {
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Value("${spring.redis.password}")
-    private String password;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
-        lettuceConnectionFactory.setPassword(password);
-        return lettuceConnectionFactory;
-    }
-
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
-    }
-
-}
+//package com.sdy.bbbb.redis;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+//
+//@Configuration
+//@EnableRedisRepositories
+//public class RedisConfig2 {
+//
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    @Value("${spring.redis.password}")
+//    private String password;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
+//        lettuceConnectionFactory.setPassword(password);
+//        return lettuceConnectionFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate() {
+//        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//        return redisTemplate;
+//    }
+//
+//}
