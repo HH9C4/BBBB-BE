f3072c6d7c076faa96a05bd685026ad5a47ee5f0
Younseo Jang <younseojj@gmail.com>
2022-12-01 22:47:41 +0900

    Fix : Login response
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/dto/response/LoginResponseDto.java
+++ src/main/java/com/sdy/bbbb/dto/response/LoginResponseDto.java
@@ -43,4 +43,16 @@ public class LoginResponseDto {
         this.reportedCount = account.getReportedCount();
         this.bookmarkList = bookmarkList;
     }
+
+    public LoginResponseDto(Account account) {
+        this.accountName = account.getAccountName();
+        this.ageRange = account.getAgeRange();
+        this.email = account.getEmail();
+        this.gender = account.getGender();
+        this.profileImage = account.getProfileImage();
+        this.naverUser = account.getNaverId() != null;
+        this.kakaoUser = account.getKakaoId() != null;
+        this.reportedCount = account.getReportedCount();
+    }
+
 }
