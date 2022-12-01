c9e9a6a41dd4b945acd26bbba289bec048182416
Younseo Jang <younseojj@gmail.com>
2022-12-01 22:19:40 +0900

    Fix : Login response 변경
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/dto/response/LoginResponseDto.java
+++ src/main/java/com/sdy/bbbb/dto/response/LoginResponseDto.java
@@ -2,8 +2,11 @@ package com.sdy.bbbb.dto.response;
 
 import com.sdy.bbbb.entity.Account;
 
+import com.sdy.bbbb.entity.Bookmark;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 
 @Getter
@@ -26,6 +29,8 @@ public class LoginResponseDto {
 
     private int reportedCount;
 
+    private List<Bookmark> bookmarkList;
+
 
     public LoginResponseDto(Account account) {
         this.accountName = account.getAccountName();
@@ -36,5 +41,6 @@ public class LoginResponseDto {
         this.naverUser = account.getNaverId() != null;
         this.kakaoUser = account.getKakaoId() != null;
         this.reportedCount = account.getReportedCount();
+        this.bookmarkList = account.getBookmarks();
     }
 }
