9baa151be37f8b64b792de9aa61356fa065fe0cf
Younseo Jang <younseojj@gmail.com>
2022-12-02 14:43:30 +0900

    Fix : server change
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,10 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# 포트포워딩
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
