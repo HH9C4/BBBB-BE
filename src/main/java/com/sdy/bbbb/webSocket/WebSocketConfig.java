2bf486508e855f0db9e2e906d18387b4999a89d9
Younseo Jang <younseojj@gmail.com>
2022-12-06 00:25:31 +0900

    Fix : webSocket
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/webSocket/WebSocketConfig.java
+++ src/main/java/com/sdy/bbbb/webSocket/WebSocketConfig.java
@@ -15,9 +15,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //클라이언트에서 보낸 메세지를 받을 prefix
-        registry.setApplicationDestinationPrefixes("/send");
+        registry.setApplicationDestinationPrefixes("/pub");
         // 해당 주소를 구독하고 있는 클라이언트들에게 메세지 전달
-        registry.enableSimpleBroker("/room");
+        registry.enableSimpleBroker("/sub");
     }
 
     @Override
