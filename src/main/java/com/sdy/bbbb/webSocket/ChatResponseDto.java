cc01ee57f7dbff0c1b36943e3307ca2a95f2203e
Younseo Jang <younseojj@gmail.com>
2022-12-06 02:44:12 +0900

    Fix : webSocket
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>
    Co-authored-by: Younseo Jang <99253403+younddo@users.noreply.github.com>
    Co-authored-by: dwon5001 <113032395+dwon5001@users.noreply.github.com>

---
+++ src/main/java/com/sdy/bbbb/webSocket/ChatResponseDto.java
@@ -0,0 +1,26 @@
+package com.sdy.bbbb.webSocket;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sdy.bbbb.entity.Account;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class ChatResponseDto {
+
+    private Long id;
+    private Long roomId;
+    private String sender;
+    private String message;
+
+    public ChatResponseDto(Chat chat) {
+        this.id = chat.getId();
+        this.roomId = chat.getRoom().getId();
+        this.sender = chat.getAccount().getAccountName();
+        this.message = chat.getMessage();
+    }
+}
