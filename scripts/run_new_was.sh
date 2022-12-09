3016a2513c1ee2562dc2d5877cd63104473788bb
Younseo Jang <younseojj@gmail.com>
2022-12-09 22:22:46 +0900

    Fix : Nginx 무중단배포
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ubuntu"
-JAR_FILE="$/yghh-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/yghh-0.0.1-SNAPSHOT.jar"
 
 # service_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호 가져오기
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
