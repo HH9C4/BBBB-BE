030945307aba91919e480c5b88809ec659f58b83
Younseo Jang <younseojj@gmail.com>
2022-12-11 03:57:30 +0900

    Fix : alarm message
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/SSE/SseService.java
+++ src/main/java/com/sdy/bbbb/SSE/SseService.java
@@ -27,7 +27,7 @@ public class SseService {
         // 503 에러를 방지하기 위한 더미 이벤트 전송
         String eventId = makeTimeIncludeId(memberId);
         sendNotification(emitter, eventId, emitterId, "EventStream Created. [userId=" + memberId + "]");
-
+        log.info("더미더미 333333333333333333333333333333333333333333333333333333333333333333333333333");
         // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
 
 //        if (hasLostData(lastEventId)) {
