182d541de60ffd2d5122a651f522b54c39d22d83
Younseo Jang <younseojj@gmail.com>
2022-12-15 11:37:26 +0900

    Test : redis
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/atester/TesterController.java
+++ src/main/java/com/sdy/bbbb/atester/TesterController.java
@@ -1,7 +1,11 @@
 package com.sdy.bbbb.atester;
 
+import com.sdy.bbbb.dto.request.ChattingDto;
 import com.sdy.bbbb.dto.response.GlobalResponseDto;
 import com.sdy.bbbb.dto.response.LoginResponseDto;
+import com.sdy.bbbb.redis.RedisData;
+import com.sdy.bbbb.redis.RedisDataRepository;
+import com.sdy.bbbb.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequiredArgsConstructor
 public class TesterController {
+
+    private final RedisDataRepository redisDataRepository;
 
     private final TestService testService;
     @GetMapping("/user/tester")
@@ -25,6 +31,11 @@ public class TesterController {
 
     @GetMapping("/coffee")
     public String test() {
+        ChattingDto chattingDto = new ChattingDto();
+        chattingDto.setMessage("테스트입니다");
+        chattingDto.setSender("테스트라구용");
+        RedisData redisData = new RedisData("test1", chattingDto);
+        redisDataRepository.save(redisData);
         return "hi";
     }
 
