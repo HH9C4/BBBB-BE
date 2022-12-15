bfca48706c796a0571e3506fac08e4f8505bcb8b
Younseo Jang <younseojj@gmail.com>
2022-12-15 16:05:45 +0900

    Fix : redis data type
    Co-authored-by: Woozy <106052832+anfrosus@users.noreply.github.com>

--- src/main/java/com/sdy/bbbb/dto/response/data/JamTop5Dto.java
+++ src/main/java/com/sdy/bbbb/dto/response/data/JamTop5Dto.java
@@ -18,7 +18,7 @@ public class JamTop5Dto {
 
     private String guNm;
 
-    private boolean isWeekend;
+    private Boolean isWeekend;
 
 
     public JamTop5Dto(JamOfWeek jamOfWeek) {
@@ -29,7 +29,7 @@ public class JamTop5Dto {
         this.isWeekend = jamOfWeek.getIsWeekend();
     }
 
-    public JamTop5Dto(Long ranking, JamDto jamDto, boolean isWeekend) {
+    public JamTop5Dto(Long ranking, JamDto jamDto, Boolean isWeekend) {
         this.ranking = ranking;
         this.areaNm = jamDto.getArea_Nm();
         this.scoreSum = jamDto.getScore_Sum();
